3b2a7151461d0e796652851fcb2fcbf23a4bdd8d
Théo Jehl <theojehl76@gmail.com>
2022-06-19 23:20:54 +0200

    Added PlatformInitPreMemory template (doesn't do anything yet)

--- Platform/Qemu/QemuOpenBoardPkg/Library/BoardInitLib/BoardInitLib.c
+++ Platform/Qemu/QemuOpenBoardPkg/Library/BoardInitLib/BoardInitLib.c
@@ -1,4 +1,9 @@
 #include "Library/BoardInitLib.h"
+#include "Library/IoLib.h"
+#include "Library/PcdLib.h"
+#include "Library/DebugLib.h"
+
+#define QEMU_IO_DEBUG_MAGIC  0xE9
 
 /**
   This board service detects the board type.
@@ -13,6 +18,7 @@ BoardDetect (
   )
 {   
     //Placeholder, must check for PIIX4 / ICH9
+    DEBUG ((DEBUG_ERROR, "BoardDetect()\n"));
     return EFI_SUCCESS;
 }
 
@@ -28,8 +34,15 @@ BoardDebugInit (
   VOID
   )
 {
-    //placeholder
-    return EFI_SUCCESS;
+    //Return EFI_SUCCESS if QEMU Debug IO ports return magic
+    if(IoRead8( PcdGet16(PcdDebugIoPort) ) == QEMU_IO_DEBUG_MAGIC ){
+        DEBUG ((DEBUG_ERROR, "QEMU IO Port ready.\n"));
+        return EFI_SUCCESS;
+    }
+    else{
+        DEBUG ((DEBUG_ERROR, "QEMU IO Port not ready / misconfigured.\n"));
+        return EFI_NOT_READY;
+    }
 }
 
 EFI_BOOT_MODE
@@ -39,5 +52,150 @@ BoardBootModeDetect (
   )
 {
     //Placeholder
+    DEBUG ((DEBUG_ERROR, "BoardBootModeDetect()\n"));
     return BOOT_WITH_FULL_CONFIGURATION;
+}
+
+/**
+  A hook for board-specific initialization prior to memory initialization.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitBeforeMemoryInit (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitBeforeMemoryInit()\n"));
+    return EFI_SUCCESS;
+}
+
+/**
+  A hook for board-specific initialization after memory initialization.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitAfterMemoryInit (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitAfterMemoryInit()\n"));
+    return EFI_SUCCESS;
+}
+
+/**
+  A hook for board-specific initialization prior to disabling temporary RAM.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitBeforeTempRamExit (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitBeforeTempRamExit()\n"));
+    return EFI_SUCCESS;
+}
+
+/**
+  A hook for board-specific initialization after disabling temporary RAM.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitAfterTempRamExit (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitAfterTempRamExit()\n"));
+    return EFI_SUCCESS;
+}
+
+/**
+  A hook for board-specific initialization prior to silicon initialization.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitBeforeSiliconInit (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitBeforeSiliconInit()\n"));
+    return EFI_SUCCESS;
+}
+
+/**
+  A hook for board-specific initialization after silicon initialization.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitAfterSiliconInit (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitAfterSiliconInit()\n"));
+    return EFI_SUCCESS;
+}
+
+/**
+  A hook for board-specific initialization after PCI enumeration.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitAfterPciEnumeration (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitAfterPciEnumeration()\n"));
+    return EFI_SUCCESS;
+}
+
+/**
+  A hook for board-specific functionality for the ReadyToBoot event.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitReadyToBoot (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitReadyToBoot()\n"));
+    return EFI_SUCCESS;
+}
+
+/**
+  A hook for board-specific functionality for the ExitBootServices event.
+
+  @retval EFI_SUCCESS   The board initialization was successful.
+  @retval EFI_NOT_READY The board has not been detected yet.
+**/
+EFI_STATUS
+EFIAPI
+BoardInitEndOfFirmware (
+  VOID
+  )
+{
+    DEBUG ((DEBUG_ERROR, "BoardInitEndOfFirmware()\n"));
+    return EFI_SUCCESS;
 }
