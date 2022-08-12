41a4eef9084a12eb78afbac14c9288ac8b1461e6
Théo Jehl <theojehl76@gmail.com>
2022-08-12 19:32:02 +0200

    QemuOpenBoardPkg: Add QemuOpenBoardPkg
    QemuOpenBoardPkg adds a MinPlatform port to Qemu x86_64
    It can boots UEFI Linux and Windows, and works on PIIX4 and Q35
    This board port provides a simple starting place for investigating edk2 and
    MinPlatform Arch.
    Currently we implement up to stage 4 of the MinPlatform spec and can boot
    Windows/Linux.
    
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Isaac Oram <isaac.w.oram@intel.com>
    Cc: Pedro Falcato <pedro.falcato@gmail.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Stefan Hajnoczi <stefanha@gmail.com>
    
    Signed-off-by: Théo Jehl <theojehl76@gmail.com>

---
+++ Platform/Qemu/QemuOpenBoardPkg/PlatformInitPei/Cpu.c
@@ -0,0 +1,56 @@
+/** @file Cpu.c
+  CPU Count initialization
+
+  Copyright (c) 2022 Théo Jehl All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include "PlatformInit.h"
+#include <IndustryStandard/Pci.h>
+#include <Library/PciCf8Lib.h>
+#include <Library/OpenQemuFwCfgLib.h>
+#include <IndustryStandard/QemuFwCfg.h>
+#include <IndustryStandard/MemoryMappedConfigurationSpaceAccessTable.h>
+#include <Library/DebugLib.h>
+#include <IndustryStandard/Acpi30.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PcdLib.h>
+#include <Library/HobLib.h>
+
+/**
+  Probe Qemu FW CFG device for current CPU count and report to MpInitLib
+
+  @return EFI_SUCCESS Detection was successful
+  @retval EFI_UNSUPPORTED Qemu FW CFG device is not present
+ */
+EFI_STATUS
+EFIAPI
+MaxCpuInit (
+  VOID
+  )
+{
+  UINT16      BootCpuCount;
+  EFI_STATUS  Status;
+
+  Status = QemuFwCfgIsPresent ();
+
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "QemuFwCfg not present, unable to detect CPU count \n"));
+    ASSERT_EFI_ERROR (Status);
+    return EFI_UNSUPPORTED;
+  }
+
+  Status = QemuFwCfgSelectItem (QemuFwCfgItemSmpCpuCount);
+
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  QemuFwCfgReadBytes (sizeof (BootCpuCount), &BootCpuCount);
+
+  PcdSet32S (PcdCpuBootLogicalProcessorNumber, BootCpuCount);
+
+  PcdSet32S (PcdCpuMaxLogicalProcessorNumber, 64);
+
+  return EFI_SUCCESS;
+}
